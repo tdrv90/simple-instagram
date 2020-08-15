03b97e1ea3b13d91b13292e717b6ad7ca4b5de25
Alexander Todorov <41835213+tdrv90@users.noreply.github.com>
2020-08-15 11:36:09 +0300

    refactor: separate Comments in component

---
+++ src/components/Comment/index.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react'
+import styles from './index.module.css'
+import { db } from '../../config/firebase'
+import firebase from 'firebase'
+
+const Comment = ({ postId, user }) => {
+    const [comments, setComments] = useState([])
+    const [comment, setComment] = useState('')
+
+
+    useEffect(() => {
+        let unsubscribe;
+        console.log(unsubscribe)
+        if (postId) {
+            unsubscribe = db
+                .collection('posts')
+                .doc(postId)
+                .collection('comments')
+                .orderBy('timestamp', 'asc')
+                .onSnapshot((snapshot) => {
+                    setComments(snapshot.docs.map((doc) => doc.data()))
+                })
+        }
+
+        return () => {
+            unsubscribe()
+        }
+    }, [postId])
+
+    const postComment = (e) => {
+        e.preventDefault()
+
+        // Input validation
+        if (!comment) {
+            return alert('Please enter comment.')
+        }
+
+        // Limit length of comment
+        if (comment.length > 300) {
+            return alert('Please enter comment less than 300 symbols.')
+        }
+
+        if (user) {
+            db.collection('posts').doc(postId).collection('comments').add({
+                text: comment,
+                username: user,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp()
+            })
+
+            setComment('')
+        }
+    }
+
+    return (
+        <>
+            <div className={styles.comment_comments}>
+                {comments.map((comment) => (
+                    <p>
+                        <strong>{comment.username}</strong> {comment.text}
+                    </p>
+                ))}
+            </div>
+            <form className={styles.comment_box}>
+                <input
+                    className={styles.comment_input}
+                    type="text"
+                    placeholder="Add a comment"
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                />
+                <button
+                    disabled={!comment}
+                    className={styles.comment_button}
+                    type="submit"
+                    onClick={postComment}
+                >
+                    Post
+                </button>
+            </form>
+        </>
+    )
+}
+
+export default Comment
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,54 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import styles from './index.module.css'
 import Avatar from '@material-ui/core/Avatar'
 import { db } from '../../config/firebase'
-import firebase from 'firebase'
+import Comment from '../Comment'
+import Likes from '../Likes'
 
 const Post = ({ postId, username, caption, imageUrl, user, myPosts }) => {
-    const [comments, setComments] = useState([])
-    const [comment, setComment] = useState('')
-
-    useEffect(() => {
-        let unsubscribe;
-        if (postId) {
-            unsubscribe = db
-                .collection('posts')
-                .doc(postId)
-                .collection('comments')
-                .orderBy('timestamp', 'asc')
-                .onSnapshot((snapshot) => {
-                    setComments(snapshot.docs.map((doc) => doc.data()))
-                })
-        }
-
-        return () => {
-            unsubscribe()
-        }
-    }, [postId])
-
-    const postComment = (e) => {
-        e.preventDefault()
-
-        // Input validation
-        if (!comment) {
-            return alert('Please enter comment.')
-        }
-
-        // Limit length of comment
-        if (comment.length > 300) {
-            return alert('Please enter comment less than 300 symbols.')
-        }
-
-        if (user) {
-            db.collection('posts').doc(postId).collection('comments').add({
-                text: comment,
-                username: user,
-                timestamp: firebase.firestore.FieldValue.serverTimestamp()
-            })
-
-            setComment('')
-        }
-    }
 
     const handleDeletePost = (e) => {
         e.preventDefault()
@@ -91,34 +48,8 @@ const Post = ({ postId, username, caption, imageUrl, user, myPosts }) => {
                     </div>
                 )}
             </div>
-            {user && (
-                <div className={styles.comment_comments}>
-                    {comments.map((comment) => (
-                        <p>
-                            <strong>{comment.username}</strong> {comment.text}
-                        </p>
-                    ))}
-                </div>
-            )}
-            {user && (
-                <form className={styles.comment_box}>
-                    <input
-                        className={styles.comment_input}
-                        type="text"
-                        placeholder="Add a comment"
-                        value={comment}
-                        onChange={(e) => setComment(e.target.value)}
-                    />
-                    <button
-                        disabled={!comment}
-                        className={styles.comment_button}
-                        type="submit"
-                        onClick={postComment}
-                    >
-                        Post
-                    </button>
-                </form>
-            )}
+            {user && (<Likes postId={postId} user={user} />)}
+            {user && (<Comment postId={postId} user={user} />)}
         </div>
     )
 }
